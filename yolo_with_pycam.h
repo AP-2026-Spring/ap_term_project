ddfb995cd7c43d31908514586e1a315422c03077
KimGeonWoo <samgkgkgk@naver.com>
2026-04-06 01:35:21 +0900

    fix: 바운드박스 비율

--- yolo_with_pycam.h
+++ yolo_with_pycam.h
@@ -32,28 +32,6 @@ inline std::vector<Detection> parse_detections_thread_safe(tflite::Interpreter* 
             std::vector<float> scores;
             std::vector<int> class_ids;
             
-            static bool debug_printed = false;
-            if (!debug_printed) {
-                printf("\n--- QUANTIZATION DEBUG ---\n");
-                printf("scale: %f, zero_point: %d\n", scale, zero_point);
-                float max_s = -1000.0f;
-                for (int idx = 0; idx < num_anchors; ++idx) {
-                    float s0 = (data[4 * num_anchors + idx] - zero_point) * scale;
-                    float s1 = (data[5 * num_anchors + idx] - zero_point) * scale;
-                    if (s0 > max_s) max_s = s0;
-                    if (s1 > max_s) max_s = s1;
-                }
-                printf("Global Max Score across all anchors: %f\n", max_s);
-                printf("First Anchor Raw: cx=%d, cy=%d, w=%d, h=%d, s0=%d, s1=%d\n", 
-                       data[0*num_anchors], data[1*num_anchors], data[2*num_anchors], data[3*num_anchors], data[4*num_anchors], data[5*num_anchors]);
-                printf("First Anchor Float: cx=%f, cy=%f, w=%f, h=%f, s0=%f, s1=%f\n",
-                       (data[0*num_anchors]-zero_point)*scale, (data[1*num_anchors]-zero_point)*scale, 
-                       (data[2*num_anchors]-zero_point)*scale, (data[3*num_anchors]-zero_point)*scale,
-                       (data[4*num_anchors]-zero_point)*scale, (data[5*num_anchors]-zero_point)*scale);
-                printf("-------------------------\n");
-                debug_printed = true;
-            }
-            
             for (int i = 0; i < num_anchors; ++i) {
                 float max_score = -1.0f;
                 int max_class = -1;
@@ -67,11 +45,11 @@ inline std::vector<Detection> parse_detections_thread_safe(tflite::Interpreter* 
                 }
                 
                 if (max_score >= 0.25f) {
-                    // Extract absolute box coordinates from the first 4 channels
-                    float cx = (data[0 * num_anchors + i] - zero_point) * scale;
-                    float cy = (data[1 * num_anchors + i] - zero_point) * scale;
-                    float w = (data[2 * num_anchors + i] - zero_point) * scale;
-                    float h = (data[3 * num_anchors + i] - zero_point) * scale;
+                    // Extract normalized box coordinates and scale them to image size
+                    float cx = ((data[0 * num_anchors + i] - zero_point) * scale) * img_width;
+                    float cy = ((data[1 * num_anchors + i] - zero_point) * scale) * img_height;
+                    float w  = ((data[2 * num_anchors + i] - zero_point) * scale) * img_width;
+                    float h  = ((data[3 * num_anchors + i] - zero_point) * scale) * img_height;
                     
                     float xmin = cx - w / 2.0f;
                     float ymin = cy - h / 2.0f;
@@ -130,26 +108,7 @@ inline std::vector<Detection> parse_detections_thread_safe(tflite::Interpreter* 
 // Current COCO label dictionary
 inline std::map<int, std::string> get_coco_label_dict() {
     return {
-        {0, "cockroach"},     {1, "mouse"},   {2, "car"},          {3, "motorbike"},
-        {4, "aeroplane"},  {5, "bus"},       {6, "train"},        {7, "truck"},
-        {8, "boat"},       {9, "traffic_light"}, {10, "fire_hydrant"}, {11, "stop_sign"},
-        {12, "parking_meter"}, {13, "bench"}, {14, "bird"},       {15, "cat"},
-        {16, "dog"},       {17, "horse"},    {18, "sheep"},       {19, "cow"},
-        {20, "elephant"},  {21, "bear"},     {22, "zebra"},       {23, "giraffe"},
-        {24, "backpack"},  {25, "umbrella"}, {26, "handbag"},     {27, "tie"},
-        {28, "suitcase"},  {29, "frisbee"},  {30, "skis"},        {31, "snowboard"},
-        {32, "sports_ball"}, {33, "kite"},   {34, "baseball_bat"}, {35, "baseball_glove"},
-        {36, "skateboard"}, {37, "surfboard"}, {38, "tennis_racket"}, {39, "bottle"},
-        {40, "wine_glass"}, {41, "cup"},     {42, "fork"},        {43, "knife"},
-        {44, "spoon"},     {45, "bowl"},    {46, "banana"},      {47, "apple"},
-        {48, "sandwich"},  {49, "orange"},  {50, "broccoli"},    {51, "carrot"},
-        {52, "hot_dog"},   {53, "pizza"},   {54, "donut"},       {55, "cake"},
-        {56, "chair"},     {57, "sofa"},    {58, "potted_plant"}, {59, "bed"},
-        {60, "dining_table"}, {61, "toilet"}, {62, "tvmonitor"}, {63, "laptop"},
-        {64, "mouse"},     {65, "remote"},  {66, "keyboard"},    {67, "cell_phone"},
-        {68, "microwave"}, {69, "oven"},    {70, "toaster"},     {71, "sink"},
-        {72, "refrigerator"}, {73, "book"}, {74, "clock"},       {75, "vase"},
-        {76, "scissors"},  {77, "teddy_bear"}, {78, "hair_drier"}, {79, "toothbrush"}
+        {0, "cockroach"},     {1, "mouse"}
     };
 }
 
